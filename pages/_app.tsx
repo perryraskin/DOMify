ff439468dd70e4d52b1b4305fd046e1f61dc35de
Perry Raskin <perryraskin@gmail.com>
2021-04-15 01:09:53 -0400

    Implements update webpage preview via virtual virtual DOM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,40 +16,24 @@ class MyApp extends App {
             content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
           ></meta>
           {/* General tags */}
-          <meta
-            key="description"
-            property="description"
-            content="Next.js TailwindCSS Starter"
-          />
-          <title key="title">Next.js TailwindCSS Starter</title>
+          <meta key="description" property="description" content="DOMify" />
+          <title key="title">DOMify</title>
           {/* OpenGraph tags */}
-          <meta
-            key="og:url"
-            property="og:url"
-            content="https://github.com/perryraskin/nextjs-tailwindcss-starter"
-          />
-          <meta
-            key="og:title"
-            property="og:title"
-            content="Next.js TailwindCSS Starter"
-          />
+          <meta key="og:url" property="og:url" content="https://domify.dev" />
+          <meta key="og:title" property="og:title" content="DOMify" />
           <meta
             key="og:description"
             property="og:description"
-            content="Next.js TailwindCSS Starter"
+            content="DOMify"
           />
           {/* <meta key="og:image" property="og:image" content="" /> */}
           <meta key="og:type" property="og:type" content="website" />
           {/* Twitter Card tags */}
-          <meta
-            key="twitter:title"
-            property="twitter:title"
-            content="Next.js TailwindCSS Starter"
-          />
+          <meta key="twitter:title" property="twitter:title" content="DOMify" />
           <meta
             key="twitter:description"
             property="twitter:description"
-            content="Next.js TailwindCSS Starter"
+            content="DOMify"
           />
           {/* <meta key="twitter:image" property="twitter:image" content="" /> */}
           <meta key="twitter:card" property="twitter:card" content="summary" />
